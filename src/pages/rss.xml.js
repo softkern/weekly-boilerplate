23778f8b1a92be627235ef1d3c96c6f5623480b1
softkern <hi@softkern.com>
2024-12-11 19:37:45 +0800

    fix: build failed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,9 @@
-import { loadRenderers } from 'astro:container'
-import { getContainerRenderer } from '@astrojs/mdx'
 import rss from '@astrojs/rss'
 import { fetchWeekly } from '@utils/weekly'
-import { experimental_AstroContainer as AstroContainer } from 'astro/container'
 import { SITE } from 'config'
 
 export const GET = async (context) => {
   const weeklies = await fetchWeekly()
-
-  const container = await AstroContainer.create({
-    renderers: await loadRenderers([getContainerRenderer()]),
-  })
 
   return rss({
     title: SITE.title,
@@ -21,7 +14,6 @@ export const GET = async (context) => {
         title: weekly.data.title,
         pubDate: weekly.data.date,
         description: weekly.data.description,
-        content: await container.renderToString((await weekly.render()).Content),
         link: `/weekly/${weekly.slug}/`,
       })),
     ),
